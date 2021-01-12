f7c64b6805658dfb67e24712b37547aa4c894bb9
April Adams <illusionlace@gmail.com>
2021-01-12 13:46:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,10 @@
 class Song
   attr_accessor :name, :artist
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self 
   end
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
@@ -15,5 +17,8 @@ class Song
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
+  def self.all 
+    @@all 
+  end 
 
 end
