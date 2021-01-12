f79fa45ee5a56e82dd8deec9bc655b56a760ca0d
April Adams <illusionlace@gmail.com>
2021-01-12 13:24:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,25 +11,25 @@ class Artist
   end
   def self.create(name)
     artist = self.new(name)
-    artist.name = name 
-    artist 
-  end 
+    artist.name = name
+    artist
+  end
   def self.find(name)
     self.all.detect{|artist| artist.name == name}
-  end 
+  end
   def self.find_or_create_by_name(name)
     self.find(name) || self.create(name)
-  end 
+  end
   def add_song(song)
-    @songs << song 
-  end 
+    @songs << song
+  end
   def print_songs
     songs.each { |song| puts song.name }
-  end 
-  def save 
-    @all << self 
-  end 
-  def songs 
-    @songs 
-  end 
+  end
+  def save
+    @all << self
+  end
+  def songs
+    @songs
+  end
 end 
