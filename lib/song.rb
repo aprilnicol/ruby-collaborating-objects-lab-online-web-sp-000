6ad066ceda457e872a3eda6e3d8ac8c482a97039
April Adams <illusionlace@gmail.com>
2021-01-12 13:46:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
 
   def initialize(name)
     @name = name
-    @@all << self 
+    @@all << self
   end
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
@@ -17,8 +17,8 @@ class Song
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end 
 
 end
